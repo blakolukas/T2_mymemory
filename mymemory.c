b88969e5bfe9724eac3854f4f1746d942ad82db1
blakolukas <lucasblacovicz@gmail.com>
2024-11-04 16:13:25 -0300

    att

--- mymemory.c
+++ mymemory.c
@@ -14,13 +14,41 @@ mymemory_t* mymemory_init(size_t size){
     return &memory;
 }
 
-void* mymemory_malloc(mymemory_t *memory, size_t size){ //first fit
-    if (!memory || size <= 0) {
+void* mymemory_alloc(mymemory_t *memory, size_t size){ //first fit
+    if(!memory || size <= 0){
         return NULL;
     }
     allocation_t *current= memory->head;
     allocation_t *prev= NULL;
-    
+    size_t count = 0;
+
+    while(current){
+        size_t gap = (size_t)((char*)current->start - (char*)memory->pool - count);
+        if (gap >= size) {
+            break;
+        }
+        count = (size_t)((char*)current->start - (char*)memory->pool) + current->size;
+        previous = current;
+        current = current->next;
+    }
+
+    if (memory->total_size - count < size) {
+        return NULL;
+    }
+
+    // Aloca o bloco e cria uma nova entrada na lista de alocações
+    allocation_t *new_allocation = (allocation_t*)(memory->pool + count);
+    new_allocation->start = (void*)(memory->pool + count);
+    new_allocation->size = size;
+    new_allocation->next = current;
+
+    if (previous) {
+        previous->next = new_allocation;
+    } else {
+        memory->head = new_allocation;
+    }
+
+    return new_allocation->start;
 
 
 }
